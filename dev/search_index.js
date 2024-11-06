89a577149ff11d53703abd3fab26f2c62c960126
Documenter.jl <documenter@juliadocs.github.io>
2024-11-06 20:45:34 +0000

    build based on cc26c86

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PhysiCellXMLRules","category":"page"},{"location":"#PhysiCellXMLRules","page":"Home","title":"PhysiCellXMLRules","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhysiCellXMLRules.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PhysiCellXMLRules]","category":"page"}]
+}
